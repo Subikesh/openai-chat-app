fe371e1ab12696f6c9af70f6da2528138c02957d
subikesh.ps <subikesh.ps@zohocorp.com>
2023-04-09 17:27:05 +0530

    Chat functionality with gradio added

--- chat.py
+++ chat.py
@@ -1,12 +1,22 @@
 import gradio
 from chat.Conversation import Conversation
 
-if __name__ == "__main__":
-    conversation = Conversation()
-    application = gradio.Interface(
-        fn=conversation.run_query,
-        inputs="text",
-        outputs="text",
-        title="Your personalized Chat GPT"
-    )
-    application.launch()
+conversation = Conversation()
+def gradio_start_chat(text):
+    global conversation
+    conversation.run_query(text)
+    messages = conversation.message_history
+    return [(messages[i].content, messages[i + 1].content) for i in range(0, len(messages)-1, 2)]
+
+
+with gradio.Blocks() as demo:
+    with gradio.Column():
+        txt = gradio.Dropdown()
+    chatbot = gradio.Chatbot()
+    with gradio.Row():
+        txt = gradio.Textbox(show_label=False, placeholder="Type your message here").style(container=False)
+        txt.submit(gradio_start_chat, txt, chatbot)
+        # txt.submit(lambda: "", None, txt)
+        txt.submit(None, None, txt, _js="() => {''}")
+
+demo.launch()
