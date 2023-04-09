ed75a541f1cf40e9c4552ec28650c27170eb4b5b
subikesh.ps <subikesh.ps@zohocorp.com>
2023-04-09 18:03:55 +0530

    Removed unused dropdown

--- chat.py
+++ chat.py
@@ -2,16 +2,16 @@ import gradio
 from chat.Conversation import Conversation
 
 conversation = Conversation()
+
+
 def gradio_start_chat(text):
     global conversation
     conversation.run_query(text)
     messages = conversation.message_history
-    return [(messages[i].content, messages[i + 1].content) for i in range(0, len(messages)-1, 2)]
+    return [(messages[i].content, messages[i + 1].content) for i in range(0, len(messages) - 1, 2)]
 
 
 with gradio.Blocks() as demo:
-    with gradio.Column():
-        txt = gradio.Dropdown()
     chatbot = gradio.Chatbot()
     with gradio.Row():
         txt = gradio.Textbox(show_label=False, placeholder="Type your message here").style(container=False)
